0a54b7356d5cbb98b9eaa0524a79fd87ef897d89
Vault 13 <v73o@yandex.ru>
2019-08-21 00:00:00 +0300

    ReadMe File and Copyright.

--- Sbm.go
+++ Sbm.go
@@ -1,26 +1,61 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright © 2019 by Vault Thirteen.
+//
+// All rights reserved. No part of this publication may be reproduced,
+// distributed, or transmitted in any form or by any means, including
+// photocopying, recording, or other electronic or mechanical methods,
+// without the prior written permission of the publisher, except in the case
+// of brief quotations embodied in critical reviews and certain other
+// noncommercial uses permitted by copyright law. For permission requests,
+// write to the publisher, addressed “Copyright Protected Material” at the
+// address below.
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// Web Site Address:	https://github.com/vault-thirteen.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 package sbm
 
-// This Package provides a Model and Methods to work with the SBM Format.
-// "SBM" is an acronym for "Simple Bit Map".
+/*----------------------------------------------------------------------------*/
+/*
 
-// The SBM Format is a Format for two-level (monochrome, black-&-white) raster
-// graphical Images.
+This Package provides a Model and Methods to work with the SBM Format.
 
-// The SBM Format is used to store a raw uncompressed Array of binary Pixels
-// together with basic meta-data describing the size of the Pixel Array.
-// SBM Format is stored in a mixed Encoding. This means that meta-data is
-// encoded as plain ASCII Text Symbols and Pixel Array is encoded using the
-// binary Format. The Array of Pixels is composed of Pixels Row by Row,
-// starting with the top Row, and having the bottom Row at the End. Each Row
-// is composed of W Pixels, where W is the Array's Width. Total Number of Rows
-// is H, where H is the Array's Height. The Array contains A Pixels total,
-// where A is the Array's Area, the multiple of W and H. Each Pixel in the
-// Array is a separate Bit, where Zero Bit (0) is black (dark color) and One
-// Bit (1) is white (light color).
+"SBM" is an acronym for "Simple Bit Map".
 
-// Due to the Limitations of current Hardware, the Order of Bits in each Byte
-// is not controlled by this Library (Package). The least significant Bit is
-// considered to be the first Bit, the most significant Bit is the last Bit.
+The SBM Format is a Format for two-level (monochrome, black-&-white) raster
+graphical Images.
+
+The SBM Format is used to store a raw uncompressed Array of binary Pixels
+together with basic meta-data describing the size of the Pixel Array. SBM
+Format is stored in a mixed Encoding. This means that meta-data is encoded as
+plain ASCII Text Symbols and Pixel Array is encoded using the binary Format.
+The Array of Pixels is composed of Pixels Row by Row, starting with the top
+Row, and having the bottom Row at the End. Each Row is composed of W Pixels,
+where W is the Array's Width. Total Number of Rows is H, where H is the Array's
+Height. The Array contains A Pixels total, where A is the Array's Area, the
+multiple of W and H. Each Pixel in the Array is a separate Bit, where Zero Bit
+(0) is black (dark color) and One Bit (1) is white (light color).
+
+Due to the Limitations of current Hardware, the Order of Bits in each Byte is
+not controlled by this Library (Package). The least significant Bit is
+considered to be the first Bit, the most significant Bit is the last Bit.
+
+The internal SBM Model Type stores both Fields: an Array of Bits (as separate
+Objects) and an Array of Bytes which are created by the Concatenation of all
+the Bits as real Machine's Bits (not as Objects). This is done for Convenience
+of various internal Manipulations with either Bits or Bytes.
+
+When the Object of the SBM Model Type is stored into the Stream, it stores
+only the Bytes Array and omits the internal Array of Bit Objects. The same
+happens when the SBM Model Type is read from the Stream -- the Bytes are
+received from the Stream, not the Bits.
+
+*/
+/*----------------------------------------------------------------------------*/
 
 // Errors.
 const (
--- SbmPixelArrayMetaDataHeaderData.go
+++ SbmPixelArrayMetaDataHeaderData.go
@@ -1,3 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright © 2019 by Vault Thirteen.
+//
+// All rights reserved. No part of this publication may be reproduced,
+// distributed, or transmitted in any form or by any means, including
+// photocopying, recording, or other electronic or mechanical methods,
+// without the prior written permission of the publisher, except in the case
+// of brief quotations embodied in critical reviews and certain other
+// noncommercial uses permitted by copyright law. For permission requests,
+// write to the publisher, addressed “Copyright Protected Material” at the
+// address below.
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// Web Site Address:	https://github.com/vault-thirteen.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 package sbm
 
 // SBM Internal Data Model: Pixel Array Meta-Data: Header Data.
